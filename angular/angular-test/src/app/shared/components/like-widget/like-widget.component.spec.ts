c011603afcb477fd93dfcc5e2a32657ad34e07e6
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-08-02 17:11:56 -0300

    Update like-widget.component.spec.ts

--- angular/angular-test/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ angular/angular-test/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,3 +1,4 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { UniqueIdService } from './../../services/unique-id/unique-id.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LikeWidgetComponent } from './like-widget.component';
@@ -8,7 +9,8 @@ describe(LikeWidgetComponent.name, () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [LikeWidgetComponent],
-            providers: [UniqueIdService]
+            providers: [UniqueIdService],
+            imports: [FontAwesomeModule]
         }).compileComponents();
 
         fixture = TestBed.createComponent(LikeWidgetComponent)
