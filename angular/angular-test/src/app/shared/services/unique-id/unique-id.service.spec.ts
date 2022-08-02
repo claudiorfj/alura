c9b976056a23c7ed8723bd14181438ec2e4f141f
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-08-02 11:24:55 -0300

    Update unique-id.service.spec.ts

--- angular/angular-test/src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ angular/angular-test/src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,14 +1,18 @@
 import { UniqueIdService } from './unique-id.service';
 
 describe(UniqueIdService.name, () => {
+
+    let service: UniqueIdService = null;
+    beforeEach(() => {
+        service = new UniqueIdService();
+    })
+    
     it(`#${UniqueIdService.prototype.generateUniqueIdeWithPrefix.name} should generate id when called with prefix`, () => {
-       const service = new UniqueIdService();
        const id = service.generateUniqueIdeWithPrefix('app');
        expect(id.startsWith('app-')).toBeTrue();
     });
 
     it(`#${UniqueIdService.prototype.generateUniqueIdeWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
-        const service = new UniqueIdService();
         const ids = new Set();
         for (let i = 0; i < 50; i++){
             ids.add(service.generateUniqueIdeWithPrefix('app'));
@@ -17,7 +21,6 @@ describe(UniqueIdService.name, () => {
     });
 
     it(`#${UniqueIdService.prototype.getNumberofGeneratedUniqueIds.name} should return the number of generatedIds when called`, () => {
-       const service = new UniqueIdService();
        service.generateUniqueIdeWithPrefix('app');
        service.generateUniqueIdeWithPrefix('app');
        expect(service.getNumberofGeneratedUniqueIds()).toBe(2);
