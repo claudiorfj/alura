2469f1fe75baa0d27893642efdce6536aae6af7f
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-08-02 16:59:03 -0300

    Update like-widget.component.spec.ts

--- angular/angular-test/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ angular/angular-test/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,13 +1,19 @@
-import { UniqueIdService } from '../../services/unique-id/unique-id.service';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LikeWidgetComponent } from './like-widget.component';
-describe(LikeWidgetComponent.name, () => {
-    let component: LikeWidgetComponent = null;
 
-    beforeEach(() => {
-        component = new LikeWidgetComponent(new UniqueIdService());
+describe(LikeWidgetComponent.name, () => {
+    let fixture: ComponentFixture<LikeWidgetComponent> = null;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [LikeWidgetComponent]
+        }).compileComponents();
+
+        fixture = TestBed.createComponent(LikeWidgetComponent)
     });
 
-    it('', () => {
-
+    it('Should create component', () => {
+        const instance = fixture.componentInstance;
+        expect(instance).toBeTruthy()
     });
 });
