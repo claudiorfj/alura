59d9df7e224dc536b4b701ee8e70c0030969b44b
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-08-02 14:45:21 -0300

    Update like.widget.module.ts

--- angular/angular-test/src/app/shared/components/like-widget/like.widget.module.ts
+++ angular/angular-test/src/app/shared/components/like-widget/like.widget.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
     declarations: [],
     imports: [
-        CommonModule
+        CommonModule,
+        FontAwesomeModule,
     ],
     exports: []
 })
