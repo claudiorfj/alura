98faefd2cd5d89067c0159bf1955675ba8e5525b
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-08-02 09:53:43 -0300

    Update unique-id.service.spec.ts

--- angular/angular-test/src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ angular/angular-test/src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,8 +1,10 @@
-describe('O artefato que queremos testar', () => {
-    it('Primeira condição que queremos testar', () => {
-        
+import { UniqueIdService } from './unique-id.service';
+describe('UniqueIdService', () => {
+    it('#generateUniqueIdeWithPrefix should generate id when called with prefix', () => {
+       const service = new UniqueIdService();
+       const id = service.generateUniqueIdeWithPrefix('app');
+       expect(id).toContain('app-')
     });
-    it('Segunda condição que queremos testar', () => {
 
-    });
 });
+
