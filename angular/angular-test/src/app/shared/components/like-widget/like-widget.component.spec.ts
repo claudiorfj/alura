cb715fe1ec56105df0bae35f15390cc0c8db7d42
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-08-02 17:13:53 -0300

    Update like-widget.component.spec.ts

--- angular/angular-test/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ angular/angular-test/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,23 +1,22 @@
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { UniqueIdService } from './../../services/unique-id/unique-id.service';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { LikeWidgetComponent } from './like-widget.component';
+import { LikeWidgetModule } from "./like-widget.module";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { UniqueIdService } from "./../../services/unique-id/unique-id.service";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { LikeWidgetComponent } from "./like-widget.component";
 
 describe(LikeWidgetComponent.name, () => {
     let fixture: ComponentFixture<LikeWidgetComponent> = null;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [LikeWidgetComponent],
-            providers: [UniqueIdService],
-            imports: [FontAwesomeModule]
+            imports: [LikeWidgetModule],
         }).compileComponents();
 
-        fixture = TestBed.createComponent(LikeWidgetComponent)
+        fixture = TestBed.createComponent(LikeWidgetComponent);
     });
 
-    it('Should create component', () => {
+    it("Should create component", () => {
         const instance = fixture.componentInstance;
-        expect(instance).toBeTruthy()
+        expect(instance).toBeTruthy();
     });
 });
