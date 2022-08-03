30901e7e5d31993b6fa2da6ee4cbb98c3ed15486
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-08-03 09:31:31 -0300

    Update like-widget.component.ts

--- angular/angular-test/src/app/shared/components/like-widget/like-widget.component.ts
+++ angular/angular-test/src/app/shared/components/like-widget/like-widget.component.ts
@@ -10,7 +10,7 @@ import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 export class LikeWidgetComponent {
     @Output() public liked = new EventEmitter<void>();
     @Input() public likes = 0;
-    @Input() public id = null;
+    @Input() public id: string = null;
     public fonts = { faThumbsUp };
 
     constructor(private uniqueIdService: UniqueIdService){}
