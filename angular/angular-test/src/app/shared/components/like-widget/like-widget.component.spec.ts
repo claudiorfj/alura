565a433ffebe2fffa557c4b51b4a6d095591cf7c
claudiorfj <93228350+claudiorfj@users.noreply.github.com>
2022-08-03 09:31:17 -0300

    Update like-widget.component.spec.ts

--- angular/angular-test/src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ angular/angular-test/src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -4,6 +4,7 @@ import { LikeWidgetComponent } from "./like-widget.component";
 
 describe(LikeWidgetComponent.name, () => {
     let fixture: ComponentFixture<LikeWidgetComponent> = null;
+    let component: LikeWidgetComponent = null;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -11,15 +12,22 @@ describe(LikeWidgetComponent.name, () => {
         }).compileComponents();
 
         fixture = TestBed.createComponent(LikeWidgetComponent);
+        component = fixture.componentInstance;
     });
 
     it("Should create component", () => {
-        const component = fixture.componentInstance;
         expect(component).toBeTruthy();
     });
 
-    it ('Should auto generate ID when id input property is missing', () => {
-        const component = fixture.componentInstance
-        expect(component.id).toBeTruthy()
-    })
+    it("Should auto generate ID when id input property is missing", () => {
+        fixture.detectChanges();
+        expect(component.id).toBeTruthy();
+    });
+
+    it("Shoudl NOT generate ID when id input property is present", () => {
+        const someId = "someId";
+        component.id = someId;
+        fixture.detectChanges();
+        expect(component.id).toBe(someId);
+    });
 });
